1732bff586259bd2b62b4ae90b08c501467ff0c0
Dave Bush <davebush@gmail.com>
2021-08-17 23:03:04 -0400

    Working commit.
    Signed-off-by: Dave Bush <davebush@gmail.com>

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "ganache",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
